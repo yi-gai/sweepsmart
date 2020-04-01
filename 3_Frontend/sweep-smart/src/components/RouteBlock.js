c747806b0f221da7b1424c6ccdb7fe14bb18eb12
Jessie Lyu <jiexinlyu@Jessies-MacBook-Pro-2.local>
2020-03-31 21:26:46 -0700

    route block

--- 3_Frontend/sweep-smart/src/components/RouteBlock.js
+++ 3_Frontend/sweep-smart/src/components/RouteBlock.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import {
-    AccessTimeRoundedIcon,
-    BlockRoundedIcon,
-    CancelRoundedIcon,
-    CheckCircleRoundedIcon,
-    HelpRoundedIcon
-} from '@material-ui/icons';
+import {AccessTime, Block, CheckCircle, Cancel} from '@material-ui/icons';
+import Grid from '@material-ui/core/Grid';
 import '../App.css';
 import './routeBlock.css';
+
+function rtBlockIcon(status) {
+    if (status === 'assigned') {
+        return (<AccessTime />);
+    } else if (status === 'disabled') {
+        return (<Block />);
+    } else if (status === 'completed') {
+        return (<CheckCircle />)
+    } else if (status === 'missed') {
+        return (<Cancel />)
+    }
+}
 
 function RouteBlock({
                         route,
                         operator,
-                        type,
                         onclick,
-                        style
+                        shift,
+                        status
                     }) {
     return (
-        <div className={`rtBlock ${style}`} onClick={onclick} type={type}>
-            <p className={`routeTxt`}>{route}</p>
-            <p className={`operatorTxt`}>{operator}</p>
+        <div className={`rtBlock rtBlock--${shift}--${status}`} onClick={onclick}>
+            <Grid container wrap="nowrap" direction="row" alignItems="center">
+                <Grid item xs={4}>
+                    {rtBlockIcon(status)}
+                </Grid>
+                <Grid item xs={8}>
+                    <p className={`routeTxt`}>{route}</p>
+                    <p className={`operatorTxt`}>{operator}</p>
+                </Grid>
+            </Grid>
         </div>
     )
 }
