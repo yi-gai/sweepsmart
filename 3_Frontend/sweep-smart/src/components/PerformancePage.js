ece2b41b3abca36d450a270bcf48fbe4a19fa188
yi-gai <yi.gai0059@gmail.com>
2020-04-20 19:45:59 -0700

    add performance page and not working chart

---
+++ 3_Frontend/sweep-smart/src/components/PerformancePage.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+// import OperatorChart from './charts';
+
+import './schedulepage.css';
+import {withStyles} from "@material-ui/styles/index";
+import API from "../API/api";
+
+const styles = theme => (
+    { }
+);
+
+
+
+class PerformancePage extends React.Component {
+
+    constructor(props) {
+		super(props);
+		this.state = {
+			onScreenData: [],
+			tab: props.tab,
+			date: props.date,
+			data:null,
+		}
+    }
+
+    componentDidMount() {
+    	API.get("/operator/week")
+      .then(res => res['data'])
+      .then(
+        (result) => {
+          console.log(result)
+          this.setState({data: result, onScreenData: result.day});
+        },
+        (error) => {
+          console.log(error)
+        }
+      )
+    }
+
+    handleChange = date => {
+    	this.props.handleDateChange(date);
+    };
+
+	render() {
+        const { classes } = this.props;
+		return (
+			<div className="content-container">
+                <div>
+                    
+                </div>
+				
+			</div>
+        );
+    }
+}
+
+export default PerformancePage;
