e0639fb955dad0861452c2df261555cabd0b5332
ANNA WALDO <anna.waldo@gmail.com>
2020-04-21 09:52:14 -0700

    absences test file

---
+++ 1_Backend/db_create_absences_test.sql
@@ -0,0 +1,19 @@
+INSERT INTO `ABSENCES` ( employee_id,date_absence,shift,time_start,time_end,comment) VALUES (2882,'2020-04-21','PM','12:00','16:00','sick');
+INSERT INTO `ABSENCES` ( employee_id,date_absence,shift,time_start,time_end) VALUES (2882,'2020-04-23','AM','8:00','12:00');
+INSERT INTO `ABSENCES` ( employee_id,date_absence,shift,time_start,time_end,comment) VALUES (1536,'2020-04-22','night','3:00','6:00','sick');
+INSERT INTO `ABSENCES` ( employee_id,date_absence,shift,time_start,time_end,comment) VALUES (1536,'2020-04-24','night','3:00','6:00','sick');
+INSERT INTO `ABSENCES` ( employee_id,date_absence,shift,time_start,time_end,comment) VALUES (14551,'2020-04-20','AM','8:00','10:00','dmv');
+INSERT INTO `ABSENCES` ( employee_id,date_absence,shift,time_start,time_end) VALUES (24252,'2020-04-20','AM','8:00','11:00');
+INSERT INTO `ABSENCES` ( employee_id,date_absence,shift,time_start,time_end) VALUES (25473,'2020-04-24','PM','12:00','16:00');
+INSERT INTO `ABSENCES` ( employee_id,date_absence,shift,time_start,time_end) VALUES (17929,'2020-04-18','night','3:00','6:00');
+
+
+INSERT INTO `ABSENCES` ( employee_id,date_absence,shift,time_start,time_end,comment) VALUES (8097,'2020-04-27','PM','12:00','16:00','sick');
+INSERT INTO `ABSENCES` ( employee_id,date_absence,shift,time_start,time_end) VALUES (2882,'2020-04-30','AM','8:00','12:00');
+INSERT INTO `ABSENCES` ( employee_id,date_absence,shift,time_start,time_end,comment) VALUES (20972,'2020-04-29','night','3:00','6:00','sick');
+INSERT INTO `ABSENCES` ( employee_id,date_absence,shift,time_start,time_end,comment) VALUES (20972,'2020-04-30','night','3:00','6:00','sick');
+INSERT INTO `ABSENCES` ( employee_id,date_absence,shift,time_start,time_end,comment) VALUES (14551,'2020-04-27','AM','8:00','10:00','dmv');
+INSERT INTO `ABSENCES` ( employee_id,date_absence,shift,time_start,time_end) VALUES (24252,'2020-04-28','AM','8:00','11:00');
+INSERT INTO `ABSENCES` ( employee_id,date_absence,shift,time_start,time_end) VALUES (25473,'2020-05-01','PM','12:00','16:00');
+INSERT INTO `ABSENCES` ( employee_id,date_absence,shift,time_start,time_end) VALUES (16843,'2020-04-25','night','3:00','6:00');
+
