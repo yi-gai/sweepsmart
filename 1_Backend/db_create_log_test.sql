363e9f7e5fa95e24a56bd1d4031597d69826a434
ANNA WALDO <anna.waldo@gmail.com>
2020-04-22 11:06:27 -0700

    route log

---
+++ 1_Backend/db_create_log_test.sql
@@ -0,0 +1,36 @@
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('1C-1',2882,7160,'2020-04-22','PM');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('1D-1',8097,7164,'2020-04-22','AM');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('7',1536,7171,'2020-04-22','AM');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('6-1',14551,7165,'2020-04-22','PM');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('6-2',20972,7167,'2020-04-22','PM');
+
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('1C-1',2882,7160,'2020-04-23','PM');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('1D-1',8097,7164,'2020-04-23','AM');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('7',1536,7171,'2020-04-23','AM');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('6-1',14551,7165,'2020-04-23','PM');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('6-2',20972,7167,'2020-04-23','PM');
+
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('1C-1',2882,7160,'2020-04-24','PM');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('1D-1',8097,7164,'2020-04-24','AM');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('7',1536,7171,'2020-04-24','AM');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('6-1',14551,7165,'2020-04-24','PM');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('6-2',20972,7167,'2020-04-24','PM');
+
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('12',16843,7167,'2020-04-25','night');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('13',17929,7166,'2020-04-25','night');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('14',16843,7167,'2020-04-26','night');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('15',17929,7166,'2020-04-26','night');
+
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('1C-1',2882,7160,'2020-04-27','PM');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('1D-1',8097,7164,'2020-04-27','AM');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('7',1536,7171,'2020-04-27','AM');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('6-1',14551,7165,'2020-04-27','PM');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('6-2',20972,7167,'2020-04-27','PM');
+
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('1C-1',2882,7160,'2020-04-28','PM');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('1D-1',8097,7164,'2020-04-28','AM');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('7',1536,7171,'2020-04-28','AM');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('6-1',14551,7165,'2020-04-28','PM');
+INSERT INTO `ROUTE_LOG` (route_id,employee_id,vehicle_id,date_swept,shift) VALUES ('6-2',20972,7167,'2020-04-28','PM');
+
+
